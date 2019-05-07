0a2b70aa1fbcf3b607dfae7657a9e719448ba8f5
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2019-05-07 12:32:18 -0700

    finished part 1 of the assignment

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
 'use strict';
 
 
-function getDogImages() {
-  let message = fetch( `https://dog.ceo/api/breeds/image/random/${collectNumberDogs()}`)
-    .then(response => response.json())
-    .then(response => response.message);
-    
-    console.log(message);
+function getDogImages(number) {
+  console.log(number);
+  fetch(`https://dog.ceo/api/breeds/image/random/${number})`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(response => {
+      console.log(response.message);
+      let message = response.message;
+      let allImages = '';
 
-  // ^^ catch invalid?? after
-  let allImages = '';
+      // put in all src images with for loop
+      for (let i = 0; i < message.length; i++) {
+        allImages += `<img src="${message[i]}" class="individual-image" />`;
+      }
 
-  // put in all src images with for loop
-  for (let i = 0; i < message.length; i++) {
-    allImages += `<img src="${message[i]}" class="individual-image" />`;
-  }
-
-  $('.js-all-the-dogs').html(allImages);
+      $('.js-all-the-dogs').html(allImages);
+      console.log(allImages);
+    })
+    .catch(response => {
+      console.log(response);
+    });
+  
 }
-
-
 
 
 function collectNumberDogs() {
 
   // get number of dogs desired from user
-  let numberDogs = $('#js-user-input').on('submit', function(event) {
+  $('#js-number-of-dogs').on('submit', function(event) {
     event.preventDefault();
-    return $(event.target).val(); 
+    let number = event.target.number.value;
+    getDogImages(number);
   });
 
-  // console.log(numberDogs);
+ 
 
   // ^ also replaced 'click' with 'submit' right??????
 
   // INCLUDE CHECK TO CONFIRM THAT NUMBER IS BETWEEN 1 AND 50
 
-  
-  if (!numberDogs) {
-    return numberDogs;
-  } else {
-    return 3;
-  }
+
   
 }
+
+$(collectNumberDogs());
